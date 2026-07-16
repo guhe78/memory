995533110cca392aaa8ac2ba30cf7b2d16b8810f
Guenter Heldt <gh1978@hotmail.de>
2026-07-16 06:52:19 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import "./styles/style.scss";
+
+init();
+
+function init() {
+  const fieldRef = document.getElementById("field");
+  if (fieldRef) {
+    fieldRef.addEventListener("click", (e) => {
+      const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement;
+      if (card) {
+        card.classList.toggle("is-flipped");
+        console.log("click");
+      }
+    });
+  }
+
+  setCards();
+}
+
+function setCards() {
+  const field = document.getElementById("field");
+
+  for (let i = 0; i < 16; i++) {
+    if (field) {
+      field.innerHTML += cardTemplate();
+    }
+  }
+}
+
+function cardTemplate() {
+  return '<button class="card"><div class="card__inner"><div class="card__face"></div><div class="card__face card__face--back">a</div></div></button>';
+}
