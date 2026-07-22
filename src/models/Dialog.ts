5c4cf1ada6a0425d86b29934de871c506d7fefd0
Guenter Heldt <gh1978@hotmail.de>
2026-07-22 17:22:08 +0200

    refactor themes

--- src/models/Dialog.ts
+++ src/models/Dialog.ts
@@ -1,11 +1,13 @@
 export class Dialog {
-  constructor(private element: HTMLElement) {}
+  constructor(private element: HTMLDialogElement) {}
 
   open() {
+    this.element.showModal();
     this.element.classList.add("open");
   }
 
   close() {
+    this.element.close();
     this.element.classList.remove("open");
   }
 }
