977a44695d0a00bea949e1dc66120c7dfcce6a5a
Guenter Heldt <gh1978@hotmail.de>
2026-07-31 11:25:46 +0200

    refactor function init

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import "./styles/main.scss";
 
 import { startButton } from "./features/home";
 
+init();
+
 /**
  * Initializes the application by setting up the start button functionality.
  */
 function init() {
   startButton();
 }
-
-init();
